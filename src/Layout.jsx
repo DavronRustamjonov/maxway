c834ddcff339597cf48d259a4ffc25b8080cc626
Rustamjonov Davron <davronalirustamjonov@gmail.com>
2024-07-17 19:56:54 +0500

    Add

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,20 +1,15 @@
 import { NavLink } from 'react-router-dom';
-import './Layout.css'; 
-import Korzinka from '/korzinka-icon.png'; 
-import Burger from '/burger.svg'; 
+import './Layout.css';
+import Korzinka from '/korzinka-icon.png';
+import Burger from '/burger.svg';
 import React, { useState } from 'react'
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
@@ -38,15 +33,41 @@ const style = {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 function Layout() {
-// Modal mui
-const [openModal, setopenModal] = React.useState(false);
+  //Send massage 
+  const [number, setNumber] = React.useState("");
+  const SendMassage = () => {
+    console.log(number);
+    const token = "6804491908:AAE8n2nu2j1Or1lxMTKi98-exL4IdgyUZlg"
+    const chat_id = '6628054450'
+    const url = `https://api.telegram.org/bot${token}/sendMessage`
+    const massage = `Uzatilgan xabar \n ${number} `
+    fetch(url, {  
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "chat_id": chat_id,
+        "text": massage
+      })
+
+    }).then(res => res.json()).then(res => {
+      alert("Xabar yuborildi.")
+    }).catch(err => {
+      console.log(err);
+    }
+    )
+  }
+
+  // Modal mui
+  const [openModal, setopenModal] = React.useState(false);
   const handleOpen = () => setopenModal(true);
   const handleClose = () => setopenModal(false);
 
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Driwer mui 
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Driwer mui 
   const [open, setOpen] = React.useState(false);
 
   const toggleDrawer = (newOpen) => () => {
@@ -56,61 +77,61 @@ const [openModal, setopenModal] = React.useState(false);
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
-        
-          <ListItem disablePadding>
-           
-            <ListItemButton>
-            
-        
-<li className='layout__item'>          
-<NavLink className='layout__link' to="/" exact>{ <HomeIcon className='drawer-icon'  />}<span className='drawer-link'>Home</span> </NavLink>
-</li>
-            </ListItemButton>
-          </ListItem>
 
-          
-          <ListItem  disablePadding>
-            <ListItemButton>
-          <li className='layout__item'> <NavLink className='layout__link' to="/filiali">
-             {<RoomIcon className='drawer-icon' />} 
-             <span className='drawer-link'> Filiali </span></NavLink></li>
-            </ListItemButton>
-          </ListItem>
-          
-          <ListItem  disablePadding>
-            <ListItemButton>
-           
-              <li className='layout__item'> <NavLink className='layout__link' to="/about">
-                {<InfoIcon className='drawer-icon'  />} 
-                <span className='drawer-link'> About </span></NavLink></li>
+        <ListItem disablePadding>
 
-            </ListItemButton>
-          </ListItem>
-          <ListItem  disablePadding>
-            <ListItemButton>
-              <li className='layout__item'> <NavLink className='layout__link' to="/contact">
-                {<CallIcon className='drawer-icon'   />} 
-                <span className='drawer-link'> Contact </span></NavLink></li>
-            </ListItemButton>
-          </ListItem>
-        
+          <ListItemButton>
+
+
+            <li className='layout__item'>
+              <NavLink className='layout__link' to="/" exact>{<HomeIcon className='drawer-icon' />}<span className='drawer-link'>Home</span> </NavLink>
+            </li>
+          </ListItemButton>
+        </ListItem>
+
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <li className='layout__item'> <NavLink className='layout__link' to="/filiali">
+              {<RoomIcon className='drawer-icon' />}
+              <span className='drawer-link'> Filiali </span></NavLink></li>
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+
+            <li className='layout__item'> <NavLink className='layout__link' to="/about">
+              {<InfoIcon className='drawer-icon' />}
+              <span className='drawer-link'> About </span></NavLink></li>
+
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <li className='layout__item'> <NavLink className='layout__link' to="/contact">
+              {<CallIcon className='drawer-icon' />}
+              <span className='drawer-link'> Contact </span></NavLink></li>
+          </ListItemButton>
+        </ListItem>
+
       </List>
       {/* <Divider /> */}
-  
+
     </Box>
   );
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   return (
     <div className='layout'>
       <div className='container layout__container'>
-                <div>
-      <Button onClick={toggleDrawer(true)}>
-        <img className='burger' src={Burger} alt="burger icon" />
- </Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer>
-    </div>
+        <div>
+          <Button onClick={toggleDrawer(true)}>
+            <img className='burger' src={Burger} alt="burger icon" />
+          </Button>
+          <Drawer open={open} onClose={toggleDrawer(false)}>
+            {DrawerList}
+          </Drawer>
+        </div>
 
 
 
@@ -127,45 +148,53 @@ const [openModal, setopenModal] = React.useState(false);
             <img className='korzinka-icon' src={Korzinka} alt="korzinka icon" />
           </li>
           <li className='layout__item'>
- 
 
 
 
-          <div>
-      <Button onClick={handleOpen} className='layout__btn'>Voyti</Button>
-      <Modal
-        open={openModal}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        className='layout-modal'
-      >
-        <Box sx={style} >
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-          
-          <h2 className="login-title">Добро пожаловать</h2>
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              <p className="login-text">Войдите с вашим номером телефона</p>
-              <form action="#">
-                <input className="login-input" type="tel" required placeholder="+998931487733" min="3" />
-                <button className="login-btn">Войти</button>
-                <ul className='login-list'>
-                <li className='login-item'>
-                  <p className='login-text'><p>У вас нет аккаунта?</p></p>
-                </li>
-                <li className='login-item'>
-                  <button className='login-link'>Регистрация</button>
-                </li>
-                </ul>
-              </form>
-          </Typography>
-        </Box>
-     
-            
-      </Modal>
 
-    </div>
+            <div>
+              <Button onClick={handleOpen} className='layout__btn'>Voyti</Button>
+              <Modal
+                open={openModal}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                className='layout-modal'
+              >
+                <Box sx={style} >
+                  <Typography id="modal-modal-title" variant="h6" component="h2">
+
+                    <h2 className="login-title">Добро пожаловать</h2>
+                  </Typography>
+                  <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    <p className="login-text">Войдите с вашим номером телефона</p>
+
+
+
+
+                    <form action="#" onSubmit={SendMassage}>
+                      <input onChange={(e) => setNumber(e.target.value)} className="login-input" type="tel" required placeholder="+998931487733" min="3" />
+                      <button type='submit' className="login-btn">Войти</button>
+                      <ul className='login-list'>
+                        <li className='login-item'>
+                          <p className='login-text'><p>У вас нет аккаунта?</p></p>
+                        </li>
+                        <li className='login-item'>
+                          <button className='login-link'>Регистрация</button>
+                        </li>
+                      </ul>
+                    </form>
+
+
+
+
+                  </Typography>
+                </Box>
+
+
+              </Modal>
+
+            </div>
 
 
           </li>
